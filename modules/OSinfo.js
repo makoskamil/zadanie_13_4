0652e491478b2c3c91c675503fb0abacb78e731b
Kamil Makoś <makos.kamil@o2.pl>
2018-05-30 09:04:38 +0200

    fix

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -16,7 +16,7 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ', : timeTransform.print();
+    console.log('Uptime: ', timeTransform.print());
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
